fb095787142f3c879592be4ea2ccf02e1019f3ec
kezia.aurelia <kezia.aurelia@eproc-gov.tech>
2024-02-22 16:13:13 +0700

    Update service upload

--- cmd/domain/file/model.go
+++ cmd/domain/file/model.go
@@ -1,9 +1,46 @@
 package file
 
+import uploaderclient "github.com/DomZippilli/gcs-proxy-cloud-function/backends/clients/uploader-client"
+
+const (
+	BULK_ACTION       = "BULK_ACTION"
+	IMAGE             = "IMAGE"
+	VIDEO             = "VIDEO"
+	DOCUMENT          = "DOCUMENT"
+	XLSX_CONTENT_TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+)
+
 type FileUploadReq struct {
-	Nama string `json: nama`
+	Type               string               `json:"type" validate:"required,oneof=BULK_ACTION IMAGE VIDEO DOCUMENT"`
+	UploadSignedUrlReq []UploadSignedUrlReq `json:"uploadSignedUrlReq" validation:"dive"`
 }
 
-type FileUploadRes struct {
-	Nama string `json:nama`
+var VALIDATION_IMAGE_METADATA = map[string]uploaderclient.ImageMetadata{
+	IMAGE: {MinSize: 1, MaxSize: 40000000, MinWidth: 300, MaxWidth: 2048, MinHeight: 300, MaxHeight: 2048},
+}
+
+var VALIDATION_VIDEO_METADATA = map[string]uploaderclient.VideoMetadata{
+	VIDEO: {MinSize: 1, MaxSize: 50000000, Duration: 120},
+}
+
+var VALIDATION_DOCUMENT_METADATA = map[string]uploaderclient.DocumentMetadata{
+	DOCUMENT:    {MinSize: 1, MaxSize: 2000000},
+	BULK_ACTION: {MinSize: 1, MaxSize: 50000000},
+}
+
+type UploadSignedUrlReq struct {
+	ContentType  string `json:"contentType" validate:"required,oneof=application/vnd.openxmlformats-officedocument.spreadsheetml.sheet image/jpg image/jpeg image/png video/mp4 video/mov application/pdf"`
+	Identifier   string `json:"identifier"`
+	FileName     string `json:"fileName"`
+	IsPublic     bool   `json:"isPublic"`
+	DocumentByte []byte `json:"documentByte"`
+}
+
+type VerifyAndDecodeTokenReq struct {
+	Token string
+}
+
+type RequestDownloadUrlReq struct {
+	Token    []string
+	IsPublic bool
 }
--- cmd/domain/file/service.go
+++ cmd/domain/file/service.go
@@ -2,12 +2,16 @@ package file
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	uploaderclient "github.com/DomZippilli/gcs-proxy-cloud-function/backends/clients/uploader-client"
+	"github.com/DomZippilli/gcs-proxy-cloud-function/backends/shared-libs/go/commonutils"
+	"github.com/ztrue/tracerr"
 )
 
 type Service interface {
-	UploadFile(ctx context.Context, input FileUploadReq) (*FileUploadRes, error)
+	UploadFile(ctx context.Context, input FileUploadReq) (string, error)
 }
 type service struct {
 	uploaderClient uploaderclient.Client
@@ -21,6 +25,58 @@ func NewService(
 	}
 }
 
-func (ths *service) UploadFile(ctx context.Context, input FileUploadReq) (*FileUploadRes, error) {
-	return nil, nil
+func (ths *service) UploadFile(ctx context.Context, input FileUploadReq) (string, error) {
+	mapUploadSignedUrlReq := make(map[string][]uploaderclient.RequestUploadSignedUrlReq)
+	for _, req := range input.UploadSignedUrlReq {
+		requestUploadSignedUrlReq := uploaderclient.RequestUploadSignedUrlReq{
+			Identifier: req.Identifier,
+			FileName:   req.FileName,
+			IsPublic:   req.IsPublic,
+		}
+		switch input.Type {
+		case IMAGE:
+			imageMetadata := VALIDATION_IMAGE_METADATA[input.Type]
+			imageMetadata.ContentType = req.ContentType
+			requestUploadSignedUrlReq.ImageMetadata = &imageMetadata
+		case VIDEO:
+			videoMetadata := VALIDATION_VIDEO_METADATA[input.Type]
+			videoMetadata.ContentType = req.ContentType
+			requestUploadSignedUrlReq.VideoMetadata = &videoMetadata
+		case DOCUMENT:
+			documentMetadata := VALIDATION_DOCUMENT_METADATA[input.Type]
+			documentMetadata.ContentType = req.ContentType
+			requestUploadSignedUrlReq.DocumentMetadata = &documentMetadata
+		case BULK_ACTION:
+			if req.ContentType != XLSX_CONTENT_TYPE {
+				return "", errors.New("invalid content type")
+			}
+			documentMetadata := VALIDATION_DOCUMENT_METADATA[input.Type]
+			documentMetadata.ContentType = req.ContentType
+			requestUploadSignedUrlReq.DocumentMetadata = &documentMetadata
+		}
+		mapUploadSignedUrlReq[req.ContentType] = append(mapUploadSignedUrlReq[req.ContentType], requestUploadSignedUrlReq)
+	}
+	uploadURL := []uploaderclient.RequestUploadSignedUrlRes{}
+	for _, req := range mapUploadSignedUrlReq {
+		resp, err := ths.uploaderClient.RequestUploadSignedUrl(
+			commonutils.ReqIDFromContext(ctx),
+			req,
+		)
+		fmt.Println(resp)
+		if err != nil {
+			return "", tracerr.Wrap(err)
+		}
+		uploadURL = append(uploadURL, resp...)
+	}
+	fmt.Println(uploadURL)
+	fileID, err := ths.uploaderClient.VerifyAndDecodeToken(uploadURL[0].JWTToken)
+	if err != nil {
+		return "", tracerr.Wrap(err)
+	}
+	err = ths.uploaderClient.UploadFile(commonutils.ReqIDFromContext(ctx), input.UploadSignedUrlReq[0].DocumentByte, uploadURL[0].SignedUrl)
+	if err != nil {
+		return "", tracerr.Wrap(err)
+	}
+
+	return fileID, nil
 }
